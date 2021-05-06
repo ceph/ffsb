07ee7f80bfea4acf486079f07dee43f95c386c5c
Xiubo Li <xiubli@redhat.com>
2021-05-06 13:39:26 +0800

    Revert "Add missing linebreak after error-message"
    This reverts commit da1232bfce338bbebab80c408a94d5fd4d182fa7.

--- ffsb_fs.c
+++ ffsb_fs.c
@@ -171,7 +171,7 @@ void *construct_ffsb_fs(void *data)
 		printf("checking existing fs: %s\n", fs->basedir);
 		ret = check_existing_fileset(fs);
 		if (ret == NULL) {
-            fprintf(stderr, "Can not reuse filesystem\n");
+            fprintf(stderr, "Can not reuse filesystem");
             exit(1);
 		}
 	} else {
